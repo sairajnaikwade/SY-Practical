edcc28146975b482069492b99304644cb18de9a2
Sairaj Naikwade <141031345+sairajnaikwade@users.noreply.github.com>
2024-10-13 20:06:19 +0530

    Update 3-Operation-On-Complex-Number.cpp

--- OOP-Practical/3-Operations-On-Complex-Number/3-Operation-On-Complex-Number.cpp
+++ OOP-Practical/3-Operations-On-Complex-Number/3-Operation-On-Complex-Number.cpp
@@ -1,119 +1,123 @@
-#include <iostream>
-#include <iomanip>
-#include <cmath>
-
+#include<iostream>
 using namespace std;
 
-class Complex {
-private:
-    float real_part;
-    float imaginary_part;
-
+class complex{    
 public:
-    Complex() {
-        real_part = 0.0;
-        imaginary_part = 0.0;
-    }
+int real;
+int imaginary;
 
-    Complex(float real_part, float imaginary_part) {
-        this->real_part = real_part;
-        this->imaginary_part = imaginary_part;
-    }
+complex(){
+  real=0;
+  imaginary=0;   
+}
 
-    Complex operator+(Complex &second_operand) {
-        Complex answer;
-        answer.real_part = real_part + second_operand.real_part;
-        answer.imaginary_part = imaginary_part + second_operand.imaginary_part;
-        return answer;
-    }
+void input(int x){
+    cout<<"Equation: "<<x+1<<endl;
+    cout<<"Enter the Real Part: ";
+    cin>>real;
+    cout<<"Enter the Imaginary Part: ";
+    cin>>imaginary;
+    display();
+}
 
-    Complex operator*(Complex &second_operand) {
-        Complex answer;
-        answer.real_part = real_part * second_operand.real_part - imaginary_part * second_operand.imaginary_part;
-        answer.imaginary_part = real_part * second_operand.imaginary_part + imaginary_part * second_operand.real_part;
-        return answer;
-    }
+void display(){
+    cout<<real<<" + i("<<imaginary<<")"<<endl;
+}
 
-    Complex operator-(Complex &second_operand) {
-        Complex answer;
-        answer.real_part = real_part - second_operand.real_part;
-        answer.imaginary_part = imaginary_part - second_operand.imaginary_part;
-        return answer;
-    }
+complex operator *(complex &a){
+   complex temp;
 
-    Complex operator/(Complex &second_operand) {
-        Complex answer;
-        float denominator = pow(second_operand.real_part, 2.0) + pow(second_operand.imaginary_part, 2.0);
-        answer.real_part = (real_part * second_operand.real_part + imaginary_part * second_operand.imaginary_part) / denominator;
-        answer.imaginary_part = (imaginary_part * second_operand.real_part - real_part * second_operand.imaginary_part) / denominator;
-        return answer;
-    }
+   temp.real=(real * a.real) - (imaginary*a.imaginary);
+   temp.imaginary=(real * a.imaginary) + (imaginary * a.real);
 
-    void show() {
-        cout << "\t [" << fixed << setprecision(2) << real_part << "] + i ["
-             << fixed << setprecision(2) << imaginary_part << "]" << endl;
-    }
+    return temp;
+}
 
-    friend ostream& operator<<(ostream &out, const Complex &a);
-    friend istream& operator>>(istream &in, Complex &a);
+complex operator +(complex &a){
+  complex temp;
+  
+  temp.real=(real+a.real);
+  temp.imaginary=(imaginary+a.imaginary);
+
+  return temp;
+}
+
+complex operator -(complex &a){
+  complex temp;
+  
+  temp.real=(real-a.real);
+  temp.imaginary=(imaginary-a.imaginary);
+
+  return temp;
+}
+
+
 };
 
-ostream& operator<<(ostream &output, const Complex &a) {
-    output << "\t ● [ Answer ] -> (" << a.real_part << ") + i (" << a.imaginary_part << ")";
-    return output;
-}
+int main(){
+    complex number[3];
+    int ch;
+    do
+    {   cout<<"Menu\n1.Addition\n2.Substraction\n3.Multiply\n4.Divide\n5.Exit\n";
+        cin>>ch;
+         switch (ch)
+        {
+         
+        case 1:
 
-istream& operator>>(istream &input, Complex &a) {
-    cout << "\t ● Enter Real Part Of the Complex Number: ";
-    input >> a.real_part;
-    cout << "\t ● Enter Imaginary Part Of the Complex Number: ";
-    input >> a.imaginary_part;
-    return input;
-}
-
-int main() {
-    Complex x, y, z;
-    int choice;
-
-    cout << "Enter Data For Complex Number 1:\n";
-    cin >> x;
-    cout << "Enter Data For Complex Number 2:\n";
-    cin >> y;
-
-    do {
-        cout << "\nOperation Menu:" << endl;
-        cout << "1. ADD Complex Number " << endl;
-        cout << "2. SUBTRACT Complex Number" << endl;
-        cout << "3. MULTIPLY Complex Number " << endl;
-        cout << "4. DIVIDE Complex Number " << endl;
-        cout << "5. EXIT" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice) {
-            case 1:
-                z = x + y;
-                cout << z << endl;
-                break;
-            case 2:
-                z = x - y;
-                cout << z << endl;
-                break;
-            case 3:
-                z = x * y;
-                cout << z << endl;
-                break;
-            case 4:
-                z = x / y;
-                cout << z << endl;
-                break;
-            case 5:
-                cout << "Exiting..." << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
+        for(int i=0;i<2;i++)
+        {
+           number[i].input(i); 
         }
-    } while (choice != 5);
 
-    return 0;
+        number[2] = number[0] + number[1];
+
+        cout<<"\nAddition is: \n";
+        number[2].display();
+        
+        break; 
+
+        case 2:
+
+         for(int i=0;i<2;i++)
+        {
+           number[i].input(i); 
+        }
+
+        number[2] = number[0] - number[1];
+
+        cout<<"\nSubstraction is: \n";
+        number[2].display();
+        
+        break; 
+
+        case 3:
+
+        for(int i=0;i<2;i++)
+        {
+           number[i].input(i); 
+        }
+
+        number[2] = number[0] * number[1];
+
+        cout<<"\nAnswer is: \n";
+        number[2].display();
+        
+        break; 
+
+        case 4:
+
+        break;
+
+        case 5:
+        break; 
+       
+        default:
+            cout<<"Wrong choice...\n";
+            break;
+        }
+    } while (ch!=5);
+    
+
+    return 0;
 }
