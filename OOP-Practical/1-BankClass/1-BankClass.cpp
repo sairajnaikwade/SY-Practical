5b0b6cb6cc2c6910c4fb397e4aba4f785d6ca011
Sairaj Naikwade <141031345+sairajnaikwade@users.noreply.github.com>
2024-10-13 20:01:55 +0530

    Update 1-BankClass.cpp

--- OOP-Practical/1-BankClass/1-BankClass.cpp
+++ OOP-Practical/1-BankClass/1-BankClass.cpp
@@ -1,136 +1,87 @@
-
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class BankUser {
-    
-private:
-    int pin;  
-
+class Bank {
 public:
+    float deposit;
     string name;
-    int balance;
-    int bank_number;
+    int account;  
+    float withdrawal;
 
-    void initializeAccount() {
-        cout << " 》Enter Name: ";
-        cin >> name;
-        cout << " 》Enter Bank Number: ";
-        cin >> bank_number;
-        cout << " 》Enter Initial Balance ( More Than ₹500 ) : ";
-        cin >> balance;
-        cout << " 》Set a 4-digit PIN: ";
-        cin >> pin;
+    void depositMoney() {
+        float depositAmount;
+        cout << "Enter the amount to deposit: ";
+        cin >> depositAmount;
+        deposit += depositAmount;
+        cout << "\n";
     }
 
-    bool isSufficientBalance(int withdraw_amount) {
-        return withdraw_amount <= balance && (balance-withdraw_amount)>500 ;
+    void accept() {
+        cout << "\nEnter the name of Account Holder: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Enter the Account number: ";
+        cin >> account;
+        cout << "Enter Your Current Balance: ";
+        cin >> deposit;
+        cout << "\n";
     }
 
-    bool validatePin(int enteredPin) {
-        return pin == enteredPin;
+    void display() {
+        cout << "\nAccount Holder: " << name;
+        cout << "\nAccount Number: " << account;
+        cout << "\nCurrent Balance: " << deposit << "\n\n";
     }
 
-    void deposit(int deposit_amount) {
-        balance += deposit_amount;
-        cout << "Deposited Amount: " << deposit_amount << "\n"
-             << "Current Balance: " << balance << endl;
-    }
-
-    void withdraw(int w_amount) {
-        if (isSufficientBalance(w_amount)) {
-            balance -= w_amount;
-            cout << "Withdrawn Amount: " << w_amount << "\n"
-                 << "Current Balance: " << balance << endl;
+    void withdraw() {
+        if (deposit <= 0) {
+            cout << "You have currently a Null Account\nCan't withdraw Money...\n";
+            return;
+        }
+        cout << "Enter the amount to withdraw: ";
+        cin >> withdrawal;
+        if (withdrawal > deposit) {
+            cout << "You don't have that much money in your Account to withdraw...\n";
         } else {
-            cout << "[ WARNING !!!!!! ] : Insufficient Balance, Can't Withdraw.\n[ Account Must Contain At least ₹ 500 ]"<<endl;
+            deposit -= withdrawal;
         }
-    }
-
-    void displayBalance() {
-        cout << " ● Account Holder Name  : " << name << endl;
-        cout << " ● Account Bank Number  : " << bank_number << endl;
-        cout << " ● Account Bank Balance : ₹" << balance << endl;
-    }
-
-    static int search(BankUser users[], int n, int target) {
-        for (int i = 0; i < n; i++) {
-            if (target == users[i].bank_number) {
-                return i;
-            }
-        }
-        return -1; 
     }
 };
 
 int main() {
-    cout << "\t\t [ SANJIVANI BANK INTERNATIONAL (SBI) ]\n\n\n";
-    int numberOfUsers;
-    cout << " 》Enter the number of users: ";
-    cin >> numberOfUsers;
-
-    BankUser users[numberOfUsers];
-
-    for (int i = 0; i < numberOfUsers; i++) {
-        cout << "\nInitializing account for user " << i + 1 << ":\n";
-        users[i].initializeAccount();
-    }
-
     int choice;
+    Bank b1;
+
     do {
-        cout << "\nOperation Menu:\n";
-        cout << "1. Deposit\n";
-        cout << "2. Withdraw\n";
-        cout << "3. Display Balance\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
+        cout << "Enter Your Choice:\n"
+             << "1. Accept New Account Holder\n"
+             << "2. Withdraw an amount\n"
+             << "3. Display information\n"
+             << "4. Deposit money\n"
+             << "5. Exit\n";
         cin >> choice;
 
-        if (choice != 4) {
-            int target;
-            cout << "Enter Bank Number: ";
-            cin >> target;
-            int index = BankUser::search(users, numberOfUsers, target);
-
-            if (index != -1) {
-                int enteredPin;
-                cout << " 》Enter your 4-digit PIN: ";
-                cin >> enteredPin;
-
-                if (users[index].validatePin(enteredPin)) {
-                    switch (choice) {
-                    case 1: {
-                        int amount;
-                        cout << " 》Enter Amount To Deposit: ";
-                        cin >> amount;
-                        users[index].deposit(amount);
-                        break;
-                    }
-                    case 2: {
-                        int w_amount;
-                        cout << " 》Enter Amount To Withdraw: ";
-                        cin >> w_amount;
-                        users[index].withdraw(w_amount);
-                        break;
-                    }
-                    case 3:
-                        cout << "\n~~~~~~~Account Details:~~~~~~~\n";
-                        users[index].displayBalance();
-                        break;
-                    default:
-                        cout << "[ WARNING !!!!! ] : Invalid choice. Please try again.\n";
-                    }
-                } else {
-                    cout << "[ WARNING !!!!! ] : Incorrect PIN. Operation failed.\n";
-                }
-            } else {
-                cout << "[ WARNING ! ] : User not found with the provided bank number.\n";
-            }
+        switch (choice) {
+            case 1:
+                b1.accept();
+                break;
+            case 2:
+                b1.withdraw();
+                break;
+            case 3:
+                b1.display();
+                break;
+            case 4:
+                b1.depositMoney();
+                break;
+            case 5:
+                cout <<"\n";
+                break;
+            default:
+                cout << "Invalid choice, please try again.\n";
         }
-    } while (choice != 4);
+    } while (choice != 5);
 
-    
-    return 0;
+ return 0;
 }
