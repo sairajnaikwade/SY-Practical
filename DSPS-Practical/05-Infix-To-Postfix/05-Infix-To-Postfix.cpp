3afe5d3d3755ce031e2cdbb6fc6a9b558ada4607
Sairaj Naikwade <141031345+sairajnaikwade@users.noreply.github.com>
2024-12-08 14:16:49 +0530

    Update 05-Infix-To-Postfix.cpp

--- DSPS-Practical/05-Infix-To-Postfix/05-Infix-To-Postfix.cpp
+++ DSPS-Practical/05-Infix-To-Postfix/05-Infix-To-Postfix.cpp
@@ -1,145 +1,65 @@
-#include<iostream>
-#include <cctype>
-#include<cstring>
+//taken from another person
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-int top=-1;
-char stack[20];
-string postfix;
-int length=-1;
-
-char topp()
-{
-	if(top==-1) {
-		return '\0';
-	}
-	return stack[top];
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
 }
 
-void push(char x) {
-	if (top == 19)
-	{
-		cout << "STACK OVERFLOW\n";
-	}
-	else
-	{
-		stack[++top] = x;
-	}
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
 }
 
-char pop()
-{
-	if (top == -1)
-	{
-		cout << "STACK UNDERFLOW\n";
-		return '\0';
-	}
-	else
-	{
-		return stack[top--];
-	}
-}
+string infixToPostfix(string infix) {
+    stack<char> s;
+    string postfix = "";
 
+    for (int i = 0; i < infix.length(); i++) {
+        char c = infix[i];
 
-int instack_priority(char x)
-{	if(x=='+' || x=='-') {
-		return 2;
-	}
-	else if(x=='*' || x=='/') {
-		return 4;
-	}
-	else if(x=='^') {
-		return 5;
-	}
-	else {
-		return 0;
-	}
-}
-
-int incoming_priority(char x)
-{	if(x=='+' || x=='-') {
-		return 1;
-	}
-	else if(x=='*' || x=='/') {
-		return 3;
-	}
-	else if(x=='^') {
-		return 6;
-	}
-	else if(x== '(') {
-		return 9;
-	}
-	else {
-		return 0;
-	}
-}
-
-
-
-
-int infix_to_postfix(char x[], char size) {
-	for (int i = 0; i < size; i++)
-	{
-		if(isalnum(x[i]))
-		{
-			postfix+=x[i];
-		}
-		else if (x[i]=='(')
-		{
-			push(x[i]);
-		}
-		else if(x[i]==')')
-		{
-			while (top != -1 && topp() != '(')
-			{
-				postfix+=pop();
-
-			}
-			if (top != -1) {
-                pop(); 
+        if (isalnum(c)) {
+            postfix += c;
+        } 
+        else if (c == '(') {
+            s.push(c);
+        } 
+        else if (c == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
             }
+            s.pop();
+        } 
+        else if (isOperator(c)) {
+            while (!s.empty() && precedence(s.top()) >= precedence(c)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
 
-		}
-		else  if(instack_priority(topp()) < incoming_priority(x[i]))
-		{
-			push(x[i]);
-		}
-		else if(instack_priority(topp()) >= incoming_priority(x[i])) {
-			while (instack_priority(topp()) >= incoming_priority(x[i]))
-			{
-				postfix+=pop();
-			}
-			push(x[i]);
-		}
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
 
-	}
-
-	while(top!=-1)
-	{
-		postfix+=pop();
-	}
-
-
-
-	return 0;
+    return postfix;
 }
 
+int main() {
+    string infix;
+    cout << "Enter an infix expression: ";
+    cin >> infix;
 
+    string postfix = infixToPostfix(infix);
+    cout << "Postfix expression: " << postfix << endl;
 
-int main()
-{
-	char infix[15];
-
-
-	cout<<"Enter the Infix Expression: "<<endl;
-	cin>>infix;
-
-	int size=strlen(infix);
-
-	infix_to_postfix(infix,size);
-
-	cout<<"POSTFIX EXPRESSION IS: "<<endl<<postfix<<endl;
-
-
-	return 0;
+    return 0;
 }
